b77c323fef6693a9d2e222ee7e603aa0e8478f07
chen.liang <cl123456>
2018-11-14 11:34:08 +0800

    工程初始化

---
+++ src/main/java/com/example/readcode/ReadcodeApplication.java
@@ -0,0 +1,12 @@
+package com.example.readcode;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReadcodeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReadcodeApplication.class, args);
+	}
+}
---
+++ src/test/java/com/example/readcode/ReadcodeApplicationTests.java
@@ -0,0 +1,16 @@
+package com.example.readcode;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ReadcodeApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
